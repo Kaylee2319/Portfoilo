82d9e3e7658a1ffb0db3729dbfa757077cf531c2
Jorge <jorgesilva1892@gmail.com>
2020-12-28 16:33:52 -0500

    adding about me section and skill cards unhovered

---
+++ portfoilo/src/components/AboutMe.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const AboutMe = () => {
+    return (
+        <div className='aboutMeSection'>
+
+            <h1 className='Heading1'>AboutMe</h1>
+            <p className='Passage1'>Passionate Navy Veteran In the middle of a career change.
+             Worked in maintenance for serval years, While in the navy I 
+             got to travel a lot and see the state of the world. After my
+            time in the navy I knew I wanted to give back to the commuinty
+            leaving the world a little bit better. Covid made me take a leap
+            of faith in pursuing what I truly enjoy technology and helping 
+            those around me. I enrolled in a wycode immersive bootcamp for 
+            both full stack development and UXUI, with both of these skill sets 
+            I can now pursue my passion by creating solutions to major problems 
+            and giving back to the community.</p>
+
+            <div className='skillcard1'><p className='cardtitle1'>General</p></div>
+            <div className='skillcard2'><p className='cardtitle2'>Design</p></div>
+            <div className='skillcard3'><p className='cardtitle3'>Frontend</p></div>
+            
+        </div>
+    )
+}
+
+export default AboutMe
--- portfoilo/src/pages/HomePage.jsx
+++ portfoilo/src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import AboutMe from '../components/AboutMe'
 
 const HomePage = () => {
   return (
   
-    <div className="homePageBanner"> 
+  <div>  <div className="homePageBanner"> 
     <div className='intro'>Hi , I am<br/>Silva<br/> A Web<br/>Designer</div>
     <div className='step1'>Research</div>
     <div className='step2'>Design</div>
     <div className='step3'>Develop</div>
+    </div>
+    <AboutMe />
     </div>
   );
 };
