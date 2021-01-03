e5507e98bcd412bb35023f0a218c106ec82244ca
Jorge <jorgesilva1892@gmail.com>
2021-01-03 18:32:42 -0500

    added icons to navbar, nav is now fixed and follows when scrolling

--- portfoilo/src/components/AboutMe.jsx
+++ portfoilo/src/components/AboutMe.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+
+
+
 const AboutMe = () => {
     return (
         <div className='aboutMeSection'>
--- portfoilo/src/components/Nav.jsx
+++ portfoilo/src/components/Nav.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import silvaNavLogo from "../images/silvaNavLogo.png" ;
-import linkedinlogo from '../images/linkedinlogo.svg'
-import githublogo from '../images/githublogo.svg'
 const Nav = () => {
     return (
     <div className='navbackground'>
@@ -10,8 +8,8 @@ const Nav = () => {
               <li className='link2'>About Me</li>
               <li > <img className='logo' src={silvaNavLogo} /> </li>
             
-              <li> <img className='linkedinlogo' src={linkedinlogo} /></li>
-              <li> <img className='githublogo' src={githublogo} /></li>
+              <li className='linkedinlogo' > </li>
+              <li className='githublogo'></li>
         </ul>         
     </div>
     )
