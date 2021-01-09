7ab0d6adca225476df1884d6cba1a51f5802d45b
Jorge <jorgesilva1892@gmail.com>
2021-01-08 20:18:15 -0500

    changing the hero image banner to svg. was having issues with the image and media queries.added the html for the new section and started the css layout for it.

--- portfoilo/src/pages/HomePage.jsx
+++ portfoilo/src/pages/HomePage.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import AboutMe from '../components/AboutMe'
+import CompassRose from '../images/CompassRose.svg'
+// import AboutMe from '../components/AboutMe'
 // import Projects from '../components/Projects';
-import hero from '../images/hero.png'
+
 
 const HomePage = () => {
   return (
-  <div>
+<div>
   <div className='Main-Sec'>
-    <img className='hero' alt='hero-banner' src={hero} />
+    
   <h1 className='Heading1'> &lt;h1&gt; <br/>Silva <br/>
 Web Designer &lt;h1&gt;
- </h1 >
- </div>
- <AboutMe />
+  </h1 >
+      <div className='Top-compass'> 
+        <div className='Html-compass'>HTML5</div>
+        <div className='Css-compass'>CSS3</div>
+      </div>
+      <div className='Bottom-compass'> 
+        <div className='Js-compass'>Javascript</div>
+        <div className='React-compass'>React</div>
+      </div>
+      <div className='Middle-compass'> 
+        <div className='Lean-compass'>Lean UX</div>
+        <div className='Design-compass'>Design Thinking</div>
+      </div>
+      <div className='Stand-alone'>Design Thinking</div>
+
+      < img className='compassRose' src={CompassRose} alt='compassrose logo'/> 
+  </div>
+ {/* <AboutMe /> */}
  {/* <Projects /> */}
-    </div>
+</div>
   );
 };
 export default HomePage;
